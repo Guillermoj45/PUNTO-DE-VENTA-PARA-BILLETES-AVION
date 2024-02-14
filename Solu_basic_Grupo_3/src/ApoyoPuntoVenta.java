71478807ead8e46d2417cee3a0b93ee469d3821e
Guillermo <gremesalgarcia@gmail.com>
2024-02-14 20:21:02 +0100

    Diccionarios hechos

--- Solu_basic_Grupo_3/src/ApoyoPuntoVenta.java
+++ Solu_basic_Grupo_3/src/ApoyoPuntoVenta.java
@@ -5,9 +5,6 @@ import java.util.Scanner;
 public class ApoyoPuntoVenta {
 
 
-    public static void guardados(array[] datos){
-
-    }
     public static void RecoDate(){
 
         // Diccionario de las abreviaciones
@@ -17,8 +14,20 @@ public class ApoyoPuntoVenta {
         IATA.put("Asturias", "OVD");
 
         // Lista de ciudades con respectivas
-        HashMap<String, ArrayList<String>> tempos = new HashMap<>();
-        tempos.put("")
+        ArrayList<String> BCN = new ArrayList<>();
+        BCN.add("BCN"); BCN.add("07:45"); BCN.add("18:55");BCN.add("90");
+
+        ArrayList<String> MAD = new ArrayList<>();
+        BCN.add("MAD"); BCN.add("08:25"); BCN.add("21:00");BCN.add("60");
+
+        ArrayList<String> BIO = new ArrayList<>();
+        BCN.add("BIO"); BCN.add("08:55"); BCN.add("20:15");BCN.add("85");
+
+        ArrayList<ArrayList<String>> TODO = new ArrayList<>();
+        TODO.add(BCN);TODO.add(MAD);TODO.add(BIO);
+
+        HashMap<String, ArrayList<ArrayList<String>>> tempos = new HashMap<>();
+        tempos.put("SVQ", TODO);
 
         Scanner input = new Scanner(System.in);
         System.out.print("Origen: ");
