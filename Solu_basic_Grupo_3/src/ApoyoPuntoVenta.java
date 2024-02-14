ab253705285ea6c29ddae6c1d09eef9c930428c0
Guillermo <gremesalgarcia@gmail.com>
2024-02-14 19:06:42 +0100

    Prints de menú

--- Solu_basic_Grupo_3/src/ApoyoPuntoVenta.java
+++ Solu_basic_Grupo_3/src/ApoyoPuntoVenta.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class ApoyoPuntoVenta {
+
+    void RecoDate(){
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Origen: ");
+        String Origen = input.nextLine();
+        System.out.print("Destino: ");
+        String Destino = input.nextLine();
+
+    }
 }
