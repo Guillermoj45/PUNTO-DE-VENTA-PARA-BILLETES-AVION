74c402775d0ada0282ef33dc87b9d78694007764
Vegalh26 <velopezhernandez@safareyes.es>
2024-02-14 20:14:53 +0100

    uwu

--- Solu_basic_Grupo_3/src/ApoyoPuntoVenta.java
+++ Solu_basic_Grupo_3/src/ApoyoPuntoVenta.java
@@ -6,7 +6,7 @@ public class ApoyoPuntoVenta {
         System.out.println("Opción 2");
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Hora de despegue (hh:mm): ");
+        System.out.print("\nHora de despegue (hh:mm): ");
         String horaDespegue = scanner.next();
         System.out.print("Duración (en minutos): ");
         int duracionMin = scanner.nextInt();
@@ -21,5 +21,34 @@ public class ApoyoPuntoVenta {
         int minFin = totalMin % 60;
 
         return String.format("%02d:%02d", horaFin, minFin);
+    }
+
+    public static void gener_tabla_nombres() {
+        System.out.println("Opción 3");
+
+        String[] meses = {
+                "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+                "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+        };
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nIntroduzca el mes en formato numérico: ");
+        int num_mes = scanner.nextInt();
+
+        System.out.println("\nLa tabla se está generando...");
+        int primer_mes = num_mes - 1;
+
+        if (primer_mes >= 0 && primer_mes < meses.length) {
+            String cadena_meses = "";
+
+            for (int i = 0; i < meses.length; i++) {
+                cadena_meses += (meses[(primer_mes + i) % meses.length] + ", ");
+            }
+
+            System.out.println("\nEl resultado solicitado es:\n" + cadena_meses);
+        }
+        else {
+            System.out.println("Mes no válido.");
+        }
     }
 }
--- Solu_basic_Grupo_3/src/PilotoAPV.java
+++ Solu_basic_Grupo_3/src/PilotoAPV.java
@@ -1,9 +1,8 @@
-import java.util.Scanner;
-
-
 public class PilotoAPV {
     public static void main(String[] args) {
         String hora_llegada = ApoyoPuntoVenta.gener_hora_llegada();
-        System.out.println("El resultado solicitado de la misma es: " + hora_llegada);
+        System.out.println("\nEl resultado solicitado de la misma es: " + hora_llegada + "\n");
+
+        ApoyoPuntoVenta.gener_tabla_nombres();
     }
 }
