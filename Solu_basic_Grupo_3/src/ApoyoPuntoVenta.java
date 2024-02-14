ab3cb66f19429b4f66ba0f8bd07cf93cac4c1976
Guillermo <gremesalgarcia@gmail.com>
2024-02-14 19:56:39 +0100

    Cambios

--- Solu_basic_Grupo_3/src/ApoyoPuntoVenta.java
+++ Solu_basic_Grupo_3/src/ApoyoPuntoVenta.java
@@ -1,15 +1,31 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class ApoyoPuntoVenta {
 
-    void RecoDate(){
 
+    public static void guardados(array[] datos){
+
+    }
+    public static void RecoDate(){
+
+        // Diccionario de las abreviaciones
+        HashMap<String,String> IATA = new HashMap<>();
+        IATA.put("Sevilla", "SVQ"); IATA.put("Madrid", "MAD"); IATA.put("Barcelona", "BCN"); IATA.put("Valencia", "VLC");
+        IATA.put("Bilbao", "BIO"); IATA.put("Málaga", "AGP"); IATA.put("A Coruña", "LCG"); IATA.put("Santander", "SDR");
+        IATA.put("Asturias", "OVD");
+
+        // Lista de ciudades con respectivas
+        HashMap<String, ArrayList<String>> tempos = new HashMap<>();
+        tempos.put("")
 
         Scanner input = new Scanner(System.in);
         System.out.print("Origen: ");
         String Origen = input.nextLine();
         System.out.print("Destino: ");
         String Destino = input.nextLine();
+
 
     }
 }
