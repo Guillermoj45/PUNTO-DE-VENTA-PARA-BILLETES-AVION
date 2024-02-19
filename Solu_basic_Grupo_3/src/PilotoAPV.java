7ebcb7ca633fdeb2df2c815554f60ca977633fb7
Guillermo <gremesalgarcia@safareyes.es>
2024-02-19 10:01:25 +0100

    Solicitud de datos arreglada

--- Solu_basic_Grupo_3/src/PilotoAPV.java
+++ Solu_basic_Grupo_3/src/PilotoAPV.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class PilotoAPV {
     public static void main(String[] args) {
-        System.out.println(ApoyoPuntoVenta.RecoDate());
+        Scanner input = new Scanner(System.in);
+        System.out.print("Origen: ");
+        String Ciudad = input.nextLine();
+        System.out.print("Destino: ");
+        String CiudadDest = input.nextLine();
+        System.out.println();
+        System.out.println(ApoyoPuntoVenta.RecoDate(Ciudad,CiudadDest));
     }
 }
