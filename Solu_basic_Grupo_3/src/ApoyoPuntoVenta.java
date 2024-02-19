ba49d29714365ab0ee4f09ba43de78e409728fbf
Pablo <pquilesgarcia@safareyes.es>
2024-02-19 12:21:13 +0100

    He añadido mi parte (métodos 1 y 4) a la rama de vega, y también he incluido el menú que hizo Daniela.

--- Solu_basic_Grupo_3/src/ApoyoPuntoVenta.java
+++ Solu_basic_Grupo_3/src/ApoyoPuntoVenta.java
@@ -1,8 +1,50 @@
 import java.util.Scanner;
+import java.util.Locale;
 import java.time.LocalTime;
+import java.time.LocalDate;
+//import java.text.ParseException;
+//import java.util.Date;
+//import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
-
+import java.util.HashMap;
 public class ApoyoPuntoVenta {
+
+    public static String transformaFecha(String[] args) {
+//        System.out.println("Opción 1");
+
+        String[] meses = {
+                "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+                "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+        };
+
+        //Recepción de valores como parámetros de ejecución
+        String arg1 = args[0];
+        String arg2 = args[1];
+        String arg3 = args[2];
+
+        System.out.println("\nSe ha recibido como parámetro de ejecución\n");
+        System.out.println("Día: " + arg1);
+        System.out.println("Mes: " + arg2);
+        System.out.println("Año: " + arg3);
+
+
+        //Formateo de la fecha
+        String fechaBasica = String.join("/", arg1, arg2, arg3);
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate date = LocalDate.parse(fechaBasica, inputFormatter);
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("d-MMMM-yyyy", new Locale("es", "ES"));
+        String fechaSalida = date.format(outputFormatter);
+
+//        String fechaBasica = String.join("/", arg1, arg2, arg3);
+//        SimpleDateFormat inputFormatter = new SimpleDateFormat("dd/MM/yyyy");
+//        Date date = inputFormatter.parse(fechaBasica);
+//        SimpleDateFormat outputFormatter = new SimpleDateFormat("d-MMMM-yyyy", new Locale("es", "ES"));
+//        String fechaSalida = outputFormatter.format(date);
+
+        return fechaSalida;
+    }
+
+
 
     public static String gener_hora_llegada() {
         System.out.println("Opción 2");
@@ -52,5 +94,30 @@ public class ApoyoPuntoVenta {
             }
         }
         return cadena_meses.substring(0, cadena_meses.length() - 2);
+    }
+
+    public static int diaMaximoMes() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nIntroduzca el mes en formato cadena: ");
+        String mesCadena = scanner.next();
+
+
+        HashMap<String, Integer> mesesDiasMaximos = new HashMap<>();
+        mesesDiasMaximos.put("Enero", 31);
+        mesesDiasMaximos.put("Febrero", 28);
+        mesesDiasMaximos.put("Marzo", 31);
+        mesesDiasMaximos.put("Abril", 30);
+        mesesDiasMaximos.put("Mayo", 31);
+        mesesDiasMaximos.put("Junio", 30);
+        mesesDiasMaximos.put("Julio", 31);
+        mesesDiasMaximos.put("Agosto", 31);
+        mesesDiasMaximos.put("Septiembre", 30);
+        mesesDiasMaximos.put("Octubre", 31);
+        mesesDiasMaximos.put("Noviembre", 30);
+        mesesDiasMaximos.put("Diciembre", 31);
+
+        int diaMaximo = mesesDiasMaximos.get(mesCadena);
+
+        return diaMaximo;
     }
 }
--- Solu_basic_Grupo_3/src/PilotoAPV.java
+++ Solu_basic_Grupo_3/src/PilotoAPV.java
@@ -1,5 +1,49 @@
+import java.util.Scanner;
 public class PilotoAPV {
     public static void main(String[] args) {
+
+        int eleccion = 1;
+        do {
+            System.out.println("\t\t\t\t\t\t\033[1mPROGRAMA BILLETE AVIÓN\033[0m");
+            System.out.println("\t\t\t\t\t\t\033[1m----------------------\033[0m");
+            System.out.println("\t\t\t\t\033[1mMENÚ\033[0m");
+            System.out.println("1.-Transformación fecha suministrada como parámetro");
+            System.out.println("2.-Generación de la hora de llegada");
+            System.out.println("3.-Generación automatizada de tabla de meses");
+            System.out.println("4.-Obtención de los días correspondientes de un mes");
+            System.out.println("5.-Recopilación información de un vuelo");
+            System.out.println("6.-Salir");
+            System.out.print("\nOpción: ");
+            Scanner entrada = new Scanner(System.in);
+            eleccion = entrada.nextInt();
+
+            switch (eleccion) {
+                case 1:
+//                    System.out.println("Opción 1");
+                    System.out.println("\nEl resultado solicitado de la misma es: " + ApoyoPuntoVenta.transformaFecha(args) + "\n");
+                    break;
+                case 2:
+                    System.out.println("Opción 2");
+                    break;
+                case 3:
+                    System.out.println("Opción 3");
+                    break;
+                case 4:
+//                    System.out.println("Opción 4");
+                    System.out.println("\nEl resultado solicitado es: " + ApoyoPuntoVenta.diaMaximoMes() + "\n");
+                    break;
+                case 5:
+                    System.out.println("Opción 5");
+                    break;
+                case 6:
+                    System.out.println("¡Hasta pronto!");
+            }
+        }
+        while (eleccion != 6);
+
+        String fechaSalida = ApoyoPuntoVenta.transformaFecha(args);
+        System.out.println("\nEl resultado solicitado de la misma es: " + fechaSalida + "\n");
+
         String hora_llegada = ApoyoPuntoVenta.gener_hora_llegada();
         System.out.println("\nEl resultado solicitado de la misma es: " + hora_llegada + "\n");
 
