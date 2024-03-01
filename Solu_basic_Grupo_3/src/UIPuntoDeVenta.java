85224590487347b6b8722137bf8dd3b81d540cee
Guillermo <gremesalgarcia@gmail.com>
2024-03-01 22:07:41 +0100

    Inicios de UI

--- Solu_basic_Grupo_3/src/UIPuntoDeVenta.java
+++ Solu_basic_Grupo_3/src/UIPuntoDeVenta.java
@@ -1,4 +1,11 @@
+import javax.swing.*;
+
 public class UIPuntoDeVenta {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Air Camela");
+        frame.setSize(600, 700);
+        // frame.add(NombreMetodo());
+    }
     // Parte Gráfica 1 Guillermo -- Se debe de hacer en un panel para facilitar la implementación con devolución del panel
 
 
