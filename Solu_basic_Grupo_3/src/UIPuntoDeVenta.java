b9a9b49d17f26de2a301819bc3d00e3331f21d33
Guillermo <gremesalgarcia@gmail.com>
2024-03-01 21:34:18 +0100

    Inicio Interfaz grafica

---
+++ Solu_basic_Grupo_3/src/UIPuntoDeVenta.java
@@ -0,0 +1,11 @@
+public class UIPuntoDeVenta {
+    // Parte Gráfica 1 Guillermo -- Se debe de hacer en un panel para facilitar la implementación
+
+
+    // Parte Gráfica 2 Pablo -- Se debe de hacer en un panel para facilitar la implementación
+
+
+    // Parte Gráfica 3 Vega -- Se debe de hacer en un panel para facilitar la implementación
+
+
+}
