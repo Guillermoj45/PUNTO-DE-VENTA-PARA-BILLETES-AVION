c32de967ad3b231db1675a2ed20a374d28d06148
Guillermo <gremesalgarcia@safareyes.es>
2024-03-04 14:00:54 +0100

    Mensajes de salida arreglados

--- Solu_basic_Grupo_3/src/UIPuntoDeVenta.java
+++ Solu_basic_Grupo_3/src/UIPuntoDeVenta.java
@@ -349,6 +349,7 @@ public class UIPuntoDeVenta {
     // Parte Gráfica 2 Pablo -- Se debe de hacer en un panel para facilitar la implementación con devolución del panel
 
 
+
     // Parte Gráfica 3 Vega -- Se debe de hacer en un panel para facilitar la implementación con devolución del panel
 
 
